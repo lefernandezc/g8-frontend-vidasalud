4ec3c17a761890f206925857c74852334d51c23c
BrayanJorquera <br.jorquera@duocuc.cl>
2026-09-11 02:59:17 -0300

    Añadido de ruta

--- frontend-vidasalud/src/app/app.routes.ts
+++ frontend-vidasalud/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './views/login/login';
+import { Atenciones } from './views/atenciones/atenciones';
+import { CatalogoPrestaciones } from './views/catalogo-prestaciones/catalogo-prestaciones';
+import { Reporteria } from './views/reporteria/reporteria';
+import { Dashboard } from './views/dashboard/dashboard';
+import { Auditoria } from './views/auditoria/auditoria';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: 'login', component: Login},
+                               {path: 'atenciones', component: Atenciones},
+                               {path: 'dashboard', component: Dashboard},
+                               {path: 'reporteria', component: Reporteria},
+                               {path: 'auditoria', component: Auditoria},
+                               {path: 'catalogo-prestaciones', component: CatalogoPrestaciones}
+];
